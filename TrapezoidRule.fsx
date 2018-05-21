a2a950a8f1c2cb6395f4e60b67eaf6cd26989afa
Maria Ines Montenegro <mimontenegro@ufm.edu>
2018-05-21 15:42:58 -0600

    Update TrapezoidRule.fsx

--- TrapezoidRule.fsx
+++ TrapezoidRule.fsx
@@ -24,3 +24,34 @@ let main() =
   Console.Write(result)
 
 main()
+
+
+open System
+
+let main() =
+
+  let f x = 
+      System.Math.Sin(x)
+
+  let rec sum (f:float -> float) (a:float) (b:float) (N:int) (iter:int) :float =
+      let h = (b - a)/N
+      let xi1 = a + h
+      let suma = h*f(xi1)
+      if (iter < N) then
+          sum f a b N iter+1
+      printfn "\nIterations: %d | root value: %f" iter suma
+  
+  let trapezoidal a b (N: int) =
+      let h = (b - a)/N
+      let first = h/2.0*(f(a)+f(b))
+      let suma = sum f a b N 0
+      let result = first + suma
+      result
+  
+  let fromA = 0.0
+  let toB = System.Math.PI
+  let counter = 100.0
+  let result = trapezoidal fromA toB counter
+  Console.Write("Result = ")
+  Console.Write(result)
+main()
