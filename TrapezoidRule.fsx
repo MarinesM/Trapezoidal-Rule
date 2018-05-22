2cc52590cb3c55b9d6bafa62c3ebe7c9bf5e494a
Maria Ines Montenegro <mimontenegro@ufm.edu>
2018-05-21 19:18:18 -0600

    Update TrapezoidRule.fsx

--- TrapezoidRule.fsx
+++ TrapezoidRule.fsx
@@ -2,56 +2,25 @@ open System
 
 let main() =
 
-  //Function to integrate
-  let f x = 
+  let rec sum_recursive (xi:float) (h:float) f (N:float) (iter:float) =
+    if iter = N then
+      xi
+    else
+      let xi_1 = xi + h*f(h*iter)
+      sum_recursive xi_1 h f N (iter+1.0)
+
+  let f x : float = 
       System.Math.Sin(x)
   
-  let trapezoidal a b N =
-      let mutable xi = a
+  let trapezoidal (a:float) (b:float) f (N:float) =
       let h = (b - a)/N
-      let mutable suma = h/2.0*(f(a)+f(b))
-      for i in 1 .. System.Convert.ToInt32(N) do
-          let mutable xi1 = xi + h
-          suma <- suma + h*f(xi1)
-          xi <- xi1
-      suma
+      let left_side = h/2.0*(f(a)+f(b))
+      left_side + sum_recursive 0.0 h f N 0.0
   
   let fromA = 0.0
   let toB = System.Math.PI
   let counter = 100.0
-  let result = trapezoidal fromA toB counter
-  Console.Write("Result = ")
-  Console.Write(result)
-
-main()
-
-
-open System
-
-let main() =
-
-  let f x = 
-      System.Math.Sin(x)
-
-  let rec sum (f:float -> float) (a:float) (b:float) (N:int) (iter:int) :float =
-      let h = (b - a)/N
-      let xi1 = a + h
-      let suma = h*f(xi1)
-      if (iter < N) then
-          sum f a b N iter+1
-      printfn "\nIterations: %d | root value: %f" iter suma
-  
-  let trapezoidal a b (N: int) =
-      let h = (b - a)/N
-      let first = h/2.0*(f(a)+f(b))
-      let suma = sum f a b N 0
-      let result = first + suma
-      result
-  
-  let fromA = 0.0
-  let toB = System.Math.PI
-  let counter = 100.0
-  let result = trapezoidal fromA toB counter
+  let result = trapezoidal fromA toB f counter
   Console.Write("Result = ")
   Console.Write(result)
 main()
